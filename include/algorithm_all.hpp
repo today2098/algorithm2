a209e57cc8b838c7e56faee5e55ca98e0955145e
Today <ipp.30042022@gmail.com>
2022-09-07 17:29:30 +0900

    [add] Create headrs including all header files

---
+++ include/algorithm_all.hpp
@@ -0,0 +1,4 @@
+#include "./algorithm/data_structure.hpp"
+#include "./algorithm/graph.hpp"
+#include "./algorithm/math_combinatorics.hpp"
+#include "./algorithm/math_number_theory.hpp"
---
+++ include/kyopro_all.hpp
@@ -0,0 +1,1 @@
+#include "./kyopro/useful.hpp"
