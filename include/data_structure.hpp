79f3b7e3140ab63d95b8df7825c5dc8456dabb80
Today <ipp.30042022@gmail.com>
2022-09-10 09:05:58 +0900

    [add] Add a class, "BIT2D0"

--- include/data_structure.hpp
+++ include/data_structure.hpp
@@ -1,5 +1,6 @@
 #include "../src/DataStructure/binary_indexed_tree.hpp"
 #include "../src/DataStructure/binary_indexed_tree_2d.hpp"
+#include "../src/DataStructure/binary_indexed_tree_2d_zero.hpp"
 #include "../src/DataStructure/binary_indexed_tree_zero.hpp"
 #include "../src/DataStructure/lazy_segment_tree.hpp"
 #include "../src/DataStructure/segment_tree.hpp"
