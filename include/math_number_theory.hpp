508acac266c5ecae467a36715aa2d1f2a44feb14
Today <ipp.30042022@gmail.com>
2022-09-10 14:52:41 +0900

    [add] Add a class, "LinerSieve"

--- include/math_number_theory.hpp
+++ include/math_number_theory.hpp
@@ -2,6 +2,7 @@
 #include "../src/MathNumberTheory/dynamic_modint.hpp"
 #include "../src/MathNumberTheory/igcd_ilcm.hpp"
 #include "../src/MathNumberTheory/is_prime.hpp"
+#include "../src/MathNumberTheory/liner_sieve.hpp"
 #include "../src/MathNumberTheory/mod_inv.hpp"
 #include "../src/MathNumberTheory/mod_pow.hpp"
 #include "../src/MathNumberTheory/modint.hpp"
