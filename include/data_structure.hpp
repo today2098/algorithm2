683dd5a53183c44efafbd17c3f584cfe049e689f
Today <ipp.30042022@gmail.com>
2022-09-10 09:50:18 +0900

    [add] Add a class, "Heap"

--- include/data_structure.hpp
+++ include/data_structure.hpp
@@ -2,6 +2,7 @@
 #include "../src/DataStructure/binary_indexed_tree_2d.hpp"
 #include "../src/DataStructure/binary_indexed_tree_2d_zero.hpp"
 #include "../src/DataStructure/binary_indexed_tree_zero.hpp"
+#include "../src/DataStructure/heap.hpp"
 #include "../src/DataStructure/lazy_segment_tree.hpp"
 #include "../src/DataStructure/segment_tree.hpp"
 #include "../src/DataStructure/union_find.hpp"
